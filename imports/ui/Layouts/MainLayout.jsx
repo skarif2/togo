090796a07512a5da194be916c54f810512dc8036
skarif2 <skarif2@gmail.com>
2016-06-03 16:03:45 +0600

    added materialize, accounts-ui and accounts-password

--- imports/ui/Layouts/MainLayout.jsx
+++ imports/ui/Layouts/MainLayout.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
+import AccountsUI from '../accounts/AccountsUI.jsx'
+
 export const MainLayout = ({ content }) => (
 	<div className = 'main-layout'>
-		<header>
-			<h2>Welcome to Togo</h2>
-		</header>
+		<div className="navbar-fixed">
+	    <nav>
+	      <div className="nav-wrapper">
+	        <a href="#!" className="brand-logo">Togo</a>
+	        <ul className="right hide-on-med-and-down">
+	          <li><a href="sass.html">All Togo</a></li>
+	          <li className="active"><a href="collapsible.html">JavaScript</a></li>
+	          <AccountsUI />
+	        </ul>
+	      </div>
+	    </nav>
+	  </div>
 		<main>
 			{content}
 		</main>
